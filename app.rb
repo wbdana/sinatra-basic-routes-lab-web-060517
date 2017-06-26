dad3e4ab65d9511a4c9700c2d62bec74bfe7c68f
wbdana <william.b.dana@gmail.com>
2017-06-26 15:06:42 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Will"
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Cucumber Slumber by Weather Report."
+  end
+
+
 end
